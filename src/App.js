4936e120ce1cd0e4324b87d054198c8ab00b8452
tienbot1 <tien.ru@mail.ru>
2024-04-04 21:49:08 +0300

    lection

--- src/App.js
+++ src/App.js
@@ -5,11 +5,26 @@ import {useState} from 'react'
 import Btn from './components/Btn/Btn';
 import Btn2 from './components/Btn2/Btn2';
 // import TestDiv from './components/TestDiv/TestDiv';
+import Title from './components/Title/Title'
 
 function App() {
-    let [text, newText] = useState('Ничего особенного, если кнопка не нажата') //всегда внутри компонента, без вложенности!!!
+    
+    //всегда внутри компонента, без вложенности!!!
     // let newText = 'Ты нажал на первую кнопку, молодец'
     // let text = 'Ничего особенного, если кнопка не нажата'
+
+    let content = function(){
+        return (
+            <div>
+                <h5>text</h5>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam magni rerum nemo omnis ab repudiandae beatae. Dolorum consequuntur aut ipsa eos harum nihil illum saepe ab! Molestiae minima adipisci inventore.</p>
+            </div>
+        )
+    }
+
+    let [text, newText] = useState(content) 
+    let [textTitle, newText2] = useState('Заголовок') 
+
     let msg1 = function(text){
         console.log('some message');
         newText(text)
@@ -17,12 +32,23 @@ function App() {
     let msg2 = function(){
         console.log('222222');
     }
-    let msg3 = function(){
-        console.log('3232323232323');
-    }
+    // let msg3 = function(){
+    //     console.log('3232323232323');
+    // }
     let parMsg = function(name, age){
         console.log(`меня зовут ${name}, мне ${age} лет`);
     }
+
+    let btnNahid = function(name){
+        console.log(`текст для кнопки ${name}`)
+    }
+
+    let changeText = function(someNew){
+        console.log(someNew);
+        newText(someNew)
+    }
+
+    let primer = <div><h1>Слово</h1></div>;
 
   return (
 
@@ -41,11 +67,13 @@ function App() {
           Learn React
         </a>
       </header>
-        <Btn action={() => msg1(111)} text='test1'/>
-        <Btn action={msg2} text='test2'/>
-        <Btn action={msg3} text='test3'/>
-        <Btn action={() => parMsg('Миша', 40)} text='test4'/>
+        <Title text={textTitle}/>
+        <Btn action={() => newText(111)} text='test1'/>
+        <Btn action={()=> changeText(primer)} text='test2'/>
+        <Btn action={() => {console.log('3232323232323')}} text='test3'/>
+        <Btn action={()=>newText2('Пока')} text='test4'/>
         <Btn2 action={msg2}>textChildren</Btn2>
+        <Btn text='кнопка для Нахида' action={()=>newText2('Прювет')}/>
         {/* <TestDiv>
             <h1>тест</h1>
             <h2><i>тест</i></h2>
--- src/components/Btn/Btn.js
+++ src/components/Btn/Btn.js
@@ -1,9 +1,17 @@
 // import './Btn.css';
 
 export default function Btn({text, action}) {
-    
+    // let btnNahid = function(){
+    //     console.log('текст для кнопки Нахида')
+    // }
+
+    let handleKeyPress = (event) => {
+        if(event.key === 'Enter'){
+          console.log('enter press here! ')
+        }
+      }
 
   return (
-    <button onClick={action}>{text}</button>
+    <button onKeyDown={handleKeyPress} onClick={action}>{text}</button>
   );
 }
